870082264d5f345d45c2cb8fbf0489831449d993
JackieDYH <1824561218@qq.com>
2020-12-10 14:39:29 +0800

    图片加载

--- src/views/image.vue
+++ src/views/image.vue
@@ -1,37 +1,39 @@
 <template>
-    <div class="image">
-        <!-- 懒加载 -->
-        <img class="preview" v-lazy="img">
+  <div class="image">
+    <!-- 懒加载 -->
+    <img class="preview" v-lazy="img" />
 
-        <div class="progressive" v-for="(item,index) in imags" :key="index">
-            <img class="preview" v-progressive="item.preview" :src="item.src">
-        </div>
-        
+    <div v-for="(item, index) in imags" :key="index">
+      <div class="progressive">
+        <img class="preview" v-progressive="item.preview" :src="item.src" />
+      </div>
     </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            img:require('../assets/image/img03.jpg'),
-            imags:[
-                {
-                    src:require('../assets/image/img01.jpg'),//小
-                    preview:require('../assets/image/img02.jpg'),//大
-                },
-                {
-                    src:require('../assets/image/img01.jpg'),
-                    preview:require('../assets/image/img03.jpg'),
-                }
-            ]
+  data() {
+    return {
+      img: require("../assets/image/img03.jpg"),
+      imags: [
+        {
+          src: require("../assets/image/img01.jpg"), //小
+          preview: require("../assets/image/img02.jpg"), //大
+        },
+        {
+            src:require('../assets/image/img01.jpg'),
+            preview:require('../assets/image/img03.jpg'),
         }
-    }
-}
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="less" scoped>
-.preview{
-    width: 100%;
+.preview {
+  width: 100%;
 }
 </style>

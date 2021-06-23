cccb210d87d1019180a0919c8e891a664d2166f9
Jackie <1824561218@qq.com>
2021-06-23 15:15:00 +0800

    不刷新页面改变语言

--- src/views/entozh.vue
+++ src/views/entozh.vue
@@ -1,7 +1,18 @@
+<!--
+ * @version: 
+ * @Description: file content
+ * @Author: Jackie
+ * @Date: 2021-06-23 15:06:16
+ * @LastEditors: Jackie
+ * @LastEditTime: 2021-06-23 15:14:45
+-->
 <template>
   <div class="wrap">
     <button @click="btn('cn')" class="btn">中文/cn</button>
     <button @click="btn('us')" class="btn">英文/us</button>
+    <hr>
+    <button @click="btns('cn')" class="btn">中文/cn</button>
+    <button @click="btns('us')" class="btn">英文/us</button>
     <p class="pm">{{$t("home.dyh")}}-{{$t("home.neirong")}}</p>
   </div>
 </template>
@@ -12,6 +23,15 @@ export default {
     console.log(this.$t)
   },
   methods: {
+    btns(t){
+      // 不刷新页面直接改变语言
+      console.log(t);
+      switch(t){
+        case 'cn': this.$i18n.locale = 'cn'; break;
+        case 'us': this.$i18n.locale = 'us'; break;
+        default : this.$i18n.locale = 'cn';
+      }
+    },
     btn(t) {
       console.log("获取",t);
       localStorage.setItem("language", t);
